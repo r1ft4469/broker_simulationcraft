d8b33b99c08ced1076eb33f386662465817847cf
r1ft4468 <pennoser@gmail.com>
2016-11-19 14:14:54 -0600

    v1.0.0.70100

--- broker_simulationcraft.lua
+++ broker_simulationcraft.lua
@@ -48,13 +48,13 @@ function LDBSimulationcraft.OnLeave()
 end
 
 function SimulationcraftMakeToolTip(self)
-	self:AddLine("Simulationcraft")
-	self:AddLine("\n")
+	self:AddLine("|cff00ff00SimulationCraft|r")
+	self:AddLine(" ")
 	SimCInvSlot = INVSLOT_HEAD
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Helm:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Helm:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -74,13 +74,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Helm:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Helm:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_NECK
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Neck:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Neck:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -100,13 +100,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Neck:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Neck:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_SHOULDER
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Shoulder:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Shoulder:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -126,13 +126,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Shoulder:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Shoulder:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_BACK
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Back:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Back:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -152,13 +152,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Back:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Back:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_CHEST
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Chest:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Chest:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -178,13 +178,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Chest:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Chest:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_WAIST
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Waist:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Waist:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -204,13 +204,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Waist:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Waist:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_LEGS
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Legs:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Legs:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -230,13 +230,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Legs:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Legs:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_FEET
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Feet:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Feet:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -256,13 +256,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Feet:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Feet:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_WRIST
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Wrist:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Wrist:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -282,13 +282,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Wrist:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Wrist:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_HAND
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Hands:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Hands:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -308,13 +308,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Hands:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Hands:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_FINGER1
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Ring:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Ring:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -334,13 +334,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Ring:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Ring:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_FINGER2
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Ring:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Ring:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -360,13 +360,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Ring:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Ring:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_TRINKET1
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Trinket:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Trinket:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -386,13 +386,13 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Trinket:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Trinket:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	SimCInvSlot = INVSLOT_TRINKET2
 	if GetInventoryItemLink("player", SimCInvSlot) ~= nil then
 		iLink = GetInventoryItemLink("player", SimCInvSlot);
 		iLvl = GetItemLevel(iLink);
-		self:AddDoubleLine("Trinket:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Trinket:",iLink.." - |cFFFFFFFF"..iLvl.."|r",1,1,1, 1,1,1);
 		if PawnVersion ~= nil then
 			Pawn_calc = PawnGetItemDataForInventorySlot(SimCInvSlot, false);
 			Pawn_output = PawnGetAllItemValues(Pawn_calc.Stats,Pawn_calc.Level,Pawn_calc.SocketBonusStats);
@@ -412,11 +412,11 @@ function SimulationcraftMakeToolTip(self)
 			end
 		end
 	else
-		self:AddDoubleLine("Trinket:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
+		self:AddDoubleLine("|cFFF9CB03Trinket:","|cFFFFFFFFNone|r",1,1,1, 1,1,1);
 	end
 	EquippedAvg = string.format("%d",select(2,GetAverageItemLevel("player")));
-	self.AddLine("\n");
-	self:AddLine("Average Item Level:|cFF0055FF"..EquippedAvg.."|r ");
+	self:AddLine(" ");
+	self:AddLine("Average Item Level: |cFF0055FF"..EquippedAvg.."|r ");
 	self:AddLine("|cff00ff00Left-click for SimulationCraft export.|r");
 	return
 end
